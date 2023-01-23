90742781295cf4f7f584139f247c9e008cce8ae7
Jerpism <jere.m@live.fi>
2023-01-23 17:02:39 +0200

    Muokattu formatointia hieman ja raakadata yhdistetään yhdeksi 16bittiseksi luvuksi

--- Team_2_ESP32/i2c_manuaalisesti/i2c_manuaalisesti.ino
+++ Team_2_ESP32/i2c_manuaalisesti/i2c_manuaalisesti.ino
@@ -5,6 +5,7 @@
  */
 #include <Arduino.h>
 #include <Wire.h>
+#include <inttypes.h>
 
 #define i2caddr 0x29 //i2c osoite anturille oletuksena 0x29 (41)
 
@@ -19,32 +20,27 @@ void setup() {
 
   /* Poistetaan CRC käytöstä */
   Wire.beginTransmission(i2caddr);
-  Wire.write(0x37); //0x3768 disable CRC
-  Wire.write(0x68);
+  Wire.write(0x37);Wire.write(0x68); //0x3768 disable CRC
   Wire.endTransmission();
 
 
   /* Self test */
   Wire.beginTransmission(i2caddr);
-  Wire.write(0x36); //0x365B Self test
-  Wire.write(0x5B);
+  Wire.write(0x36);Wire.write(0x5B); //0x365B Self test
   Wire.endTransmission();
 
   delay(100); //Tarvittu viive (voi olla lopuksi pienempikin, 100 toimii nyt)
 
   /* Luetaan vastaus (00 == Kaikki OK) */
-  Wire.requestFrom(0x29,2);
+  Wire.requestFrom(i2caddr,2);
   while(Wire.available()) {
     Serial.print(Wire.read());
   }
 
   /* Asetetaan kaasu CO2 ilmassa 0-100% */
   Wire.beginTransmission(i2caddr);
-  Wire.write(0x36); //0x3165 set binary gas
-  Wire.write(0x15);
-
-  Wire.write(0x00); //arg 0x0001 CO2 in air 0 to 100
-  Wire.write(0x01);
+  Wire.write(0x36);Wire.write(0x15); //0x3165 set binary gas
+  Wire.write(0x00);Wire.write(0x01); //arg 0x0001 CO2 in air 0 to 100
   Wire.endTransmission();
 
 }
@@ -54,18 +50,28 @@ void loop() {
 
   /* Mitataan kaasu */
   Wire.beginTransmission(i2caddr);
-  Wire.write(0x36); //0x3639 measure gas concentration
-  Wire.write(0x39);
+  Wire.write(0x36);Wire.write(0x39); //0x3639 measure gas concentration
   Wire.endTransmission();
 
   delay(100); //Tarvitaan viive ennen lukua (saattaa toimia pienemmälläkin)
-
+  
+  int i = 0;
+  uint8_t bytes[2]={0,0}; //Voidaan laajentaa jos halutaan lukea myös lämpötila
+  uint16_t result=0;
+  
   /* Luetaan saadut tiedot */
-  Wire.requestFrom(0x29, 2); //luetaan 2 tavua, tiedot kaasusta (ei lämpötilaa)
+  Wire.requestFrom(i2caddr, 2); //luetaan 2 tavua, tiedot kaasusta (ei lämpötilaa)
   while(Wire.available()){
-    Serial.print(byte(Wire.read()));
-    Serial.print(" ");
+    bytes[i] = Wire.read(); //Data tulee MSB ensin 
+    ++i;
   }
-  Serial.println();
+  //liitetään molemmat arvot samaan
+  result = ((uint16_t)bytes[0]<<8) | (uint16_t)bytes[1]; //rullataan ensimmäistä tavua vasemmalle 8 paikkaa ja sitten bittitason OR, jotta saadaan yksi 16 bittinen arvo.
+  Serial.println(result);
+  //Tarkistusta varten
+  /*Serial.print("\t");
+  Serial.print(bytes[0]);
+  Serial.print(" ");
+  Serial.println(bytes[1]);*/
 
 }
