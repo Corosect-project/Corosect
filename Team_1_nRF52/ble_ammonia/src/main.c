adb8a9c2a9a64024f388b3f02b8c402600351885
Astru43 <45196326+Astru43@users.noreply.github.com>
2023-01-26 08:48:10 +0200

    Add sensor reading with spi

--- Team_1_nRF52/ble_ammonia/src/main.c
+++ Team_1_nRF52/ble_ammonia/src/main.c
@@ -5,13 +5,18 @@
  */
 
 #include <dk_buttons_and_leds.h>
+#include <nrf52.h>
+#include <nrfx_spi.h>
 #include <zephyr/bluetooth/bluetooth.h>
 #include <zephyr/bluetooth/conn.h>
 #include <zephyr/kernel.h>
 
-volatile bool quit = false;
-
 #define ERROR(err) (err < 0)
+#define CS_PIN 27
+#define SCK_PIN 26
+#define SDO_PIN 2
+
+volatile bool quit = false;
 
 void button_handler(uint32_t state, uint32_t has_changed) {
   if (DK_BTN2_MSK == has_changed) {
@@ -34,6 +39,68 @@ void bt_ready() {
   printk("%d\n", err);
 }
 
+static inline void set_cs() {
+  nrf_gpio_pin_set(CS_PIN);
+}
+
+static inline void clear_cs() {
+  nrf_gpio_pin_clear(CS_PIN);
+}
+
+static inline void clock_pulse() {
+  nrf_gpio_pin_clear(SCK_PIN);
+  k_sleep(K_MSEC(1));
+  nrf_gpio_pin_set(SCK_PIN);
+  k_sleep(K_MSEC(1));
+}
+
+int read_an3_data(nrfx_spi_t *instance, nrfx_spi_xfer_desc_t *transf) {
+  uint32_t ready = 1;
+  uint32_t res = 0;
+  do {
+    clear_cs();
+    ready = nrf_gpio_pin_read(SDO_PIN);
+    if (ready != 0) k_sleep(K_MSEC(250));
+    set_cs();
+    printk("0x%x\n", ready);
+  } while (ready != 0);
+
+  clear_cs();
+  k_sleep(K_MSEC(1));
+    struct
+    {
+      uint8_t OL : 1;
+      uint8_t OH : 1;
+      uint8_t SIGN : 1;
+    } flags;
+
+    for (size_t i = 0; i < 24; i++) {
+      nrf_gpio_pin_clear(SCK_PIN);
+      k_sleep(K_MSEC(1));
+      nrf_gpio_pin_set(SCK_PIN);
+      switch (i) {
+        case 0:
+          flags.OL = nrf_gpio_pin_read(SDO_PIN);
+          break;
+        case 1:
+          flags.OH = nrf_gpio_pin_read(SDO_PIN);
+          break;
+        case 2:
+          flags.SIGN = nrf_gpio_pin_read(SDO_PIN);
+          break;
+        default:
+          res <<= 1;
+          res |= nrf_gpio_pin_read(SDO_PIN);
+          break;
+      }
+      k_sleep(K_MSEC(1));
+    }
+  // int err = nrfx_spi_xfer(instance, transf, 0);
+  set_cs();
+  printk("%d %d %d\n", flags.OL, flags.OH, flags.SIGN);
+  return res;
+}
+
 void main(void) {
   printk("Hello World! %s\n", CONFIG_BOARD);
   if (ERROR(dk_leds_init()))
@@ -45,12 +112,41 @@ void main(void) {
   if (ERROR(err)) printk("Error enabling BT %d", err);
   bt_ready();
 
+  nrfx_spi_t instance = NRFX_SPI_INSTANCE(0);
+  nrfx_spi_config_t config = NRFX_SPI_DEFAULT_CONFIG(SCK_PIN, NRFX_SPI_PIN_NOT_USED, SDO_PIN, NRFX_SPI_PIN_NOT_USED);
+  config.mode = NRF_SPI_MODE_3;
+  config.frequency = SPI_FREQUENCY_FREQUENCY_K125;
+  nrf_gpio_cfg_output(CS_PIN);
+  nrf_gpio_cfg_input(SDO_PIN, NRF_GPIO_PIN_PULLUP);
+  nrf_gpio_cfg_output(SCK_PIN);
+  set_cs();
+  nrf_gpio_pin_set(SCK_PIN);
+  // nrfx_spi_init(&instance, &config, NULL, NULL);
+
+  uint8_t rx_buff[3];
+  nrfx_spi_xfer_desc_t transf = NRFX_SPI_XFER_RX(rx_buff, 3);
+
+  // clear_cs();
+  // for (size_t i = 0; i < 24; i++) clock_pulse();
+  // set_cs();
+  // k_sleep(K_MSEC(2000));
+  // clear_cs();
+  // k_sleep(K_MSEC(1));
+  // set_cs();
+
   while (!quit) {
-    dk_set_leds(DK_ALL_LEDS_MSK);
-    for (size_t i = 0; i < 4; i++) {
-      k_sleep(K_MSEC(1000));
-      dk_set_led(i, 0);
-    }
+    int err = read_an3_data(&instance, &transf);
+    printk("ERROR_CODE: %d\n", err);
+    if (err == NRFX_SUCCESS)
+      printk("DATA: 0x%x 0x%x 0x%x\n", rx_buff[0], rx_buff[1], rx_buff[2]);
+    // // dk_set_leds(DK_ALL_LEDS_MSK);
+    // // for (size_t i = 0; i < 4; i++) {
+    // //   k_sleep(K_MSEC(1000));
+    // //   dk_set_led(i, 0);
+    // // }
+    // clear_cs();
+    // k_sleep(K_MSEC(2000));
+    // set_cs();
     k_sleep(K_MSEC(2000));
   }
   printk("QUIT\n");
