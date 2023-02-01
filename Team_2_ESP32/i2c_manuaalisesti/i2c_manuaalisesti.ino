303eaa88f6c6a3a67c3ff344da345352bcd01d6a
jerpism <jere.m@live.fi>
2023-02-01 12:04:24 +0200

    Pieniä muokkauksia. Datan lähetykselle tehty erillinen funktio. Lisätty lämpötilan mittaus

--- Team_2_ESP32/i2c_manuaalisesti/i2c_manuaalisesti.ino
+++ Team_2_ESP32/i2c_manuaalisesti/i2c_manuaalisesti.ino
@@ -10,22 +10,25 @@
 #include <PubSubClient.h>
 
 #define co2_addr 0x29 //i2c osoite co2 anturille, oletuksena 0x29 (41)
-#define WL_MAX_ATTEMPTS 4 //Maksimimäärä sallittuja yrityksiä wlanin yhdistämiselle
+#define WL_MAX_ATTEMPTS 5 //Maksimimäärä sallittuja yrityksiä wlanin yhdistämiselle
 
 enum{
-  WL_NOT_CONNECTED, //Nettiä ei yhdistetty eikä ole yritetty
-  WL_CONNECT_SUCCESS, //Netti on yhdistetty
-  WL_CONNECT_ERROR //Nettiä ei ole yhdistetty ja on yritetty
+  WLAN_NOT_CONNECTED, //Nettiä ei yhdistetty eikä ole yritetty
+  WLAN_CONNECT_SUCCESS, //Netti on yhdistetty
+  WLAN_CONNECT_ERROR //Nettiä ei ole yhdistetty ja on yritetty
 } WLAN_STATE;
 
 //Wifi
-char ssid[]="";
+char ssid[]="panoulu";
 char pass[]="";
 
 //MQTT
-char broker[]="10.0.0.175";
+char broker[]="100.71.190.209";
 int port = 1883;
-char topic[]="test";
+char devname[] = "esp32";
+
+char co2_topic[]="co2";
+char temp_topic[]="temp";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -38,46 +41,52 @@ void setup() {
 
   Wire.begin();
 
-  WLAN_STATE = WL_NOT_CONNECTED; //Aloitustilassa oletetaan wlan pois päältä
+  WLAN_STATE = WLAN_NOT_CONNECTED; //Aloitustilassa oletetaan wlan pois päältä
 
   /* Poistetaan CRC käytöstä */
-  Wire.beginTransmission(i2caddr);
+  Wire.beginTransmission(co2_addr);
   Wire.write(0x37);Wire.write(0x68); //0x3768 disable CRC
   Wire.endTransmission();
 
-
   /* Self test */
   Serial.print("Itsetesti (00 == OK): ");
-  Wire.beginTransmission(i2caddr);
+  Wire.beginTransmission(co2_addr);
   Wire.write(0x36);Wire.write(0x5B); //0x365B Self test
   Wire.endTransmission();
 
   delay(100); //Tarvittu viive (voi olla lopuksi pienempikin, 100 toimii nyt)
 
   /* Luetaan vastaus (00 == Kaikki OK) */
-  Wire.requestFrom(i2caddr,2);
+  Wire.requestFrom(co2_addr,2);
   while(Wire.available()) {
     Serial.print(Wire.read());
   }
   Serial.println();
 
   /* Asetetaan kaasu CO2 ilmassa 0-100% */
-  Wire.beginTransmission(i2caddr);
+  Wire.beginTransmission(co2_addr);
   Wire.write(0x36);Wire.write(0x15); //0x3165 set binary gas
   Wire.write(0x00);Wire.write(0x01); //arg 0x0001 CO2 in air 0 to 100
   Wire.endTransmission();
 
   checkStateAndConnect();
-  connectMQTT();
+  //connectMQTT();
 }
 
 void checkStateAndConnect(){
-  if(WLAN_STATE == WL_NOT_CONNECTED){
+  if(WLAN_STATE == WLAN_NOT_CONNECTED){
     enableWifi(); //Oletetaan, että netti löytyy sieltä mistä pitääkin ja yhdistetään
-  }else if(WLAN_STATE == WL_CONNECT_ERROR){
+  }else if(WLAN_STATE == WLAN_CONNECT_ERROR){
     delay(5000); //Odotetaan pitempi aika jospa se netti tulisi sillä aikaa takaisin
     enableWifi();
   }
+}
+
+void disableWifi(){
+  //WiFi pois päältä ja samalla katkeaa MQTT yhteys
+  WiFi.disconnect(true); 
+  WiFi.mode(WIFI_OFF);
+  WLAN_STATE = WLAN_NOT_CONNECTED;  
 }
 
 void enableWifi(){
@@ -87,100 +96,86 @@ void enableWifi(){
 
   Serial.println("Käynnistetään wlan");
   WiFi.begin(ssid,pass);
-  
+
+  /* En ole täysin tyytyväinen tähän, sisältää luultavasti jonkinlaisen virheen/virheitä.
+   * Vaatii siis varmaan uudelleenkirjoituksen joskus pian(tm)*/
   while(WiFi.status() != WL_CONNECTED){
-     if(attempts >= WL_MAX_ATTEMPTS){ 
-      WLAN_STATE = WL_CONNECT_ERROR;
+     if(attempts >= WL_MAX_ATTEMPTS){ //Yritysten määrä ylittyi, poistutaan loopista ja asetetaan tilaksi WLAN_CONNECT_ERROR
+      WLAN_STATE = WLAN_CONNECT_ERROR;
       break;
     }
-    delay(1000);
     Serial.print(".");
+    delay(1000);
     ++attempts;
   }
 
-  if(WLAN_STATE == WL_CONNECT_ERROR){
+  if(WLAN_STATE == WLAN_CONNECT_ERROR){ //Yhdistäminen epäonnistui, kerrotaan yritysten määrä
     Serial.print("Wlaniin yhdistäminen epäonnistui, yrityksiä: ");
     Serial.println(attempts);
-  }else{
+  }else{ //Toinen vaihtoehto on, että yhdistäminen onnistui, joten asetetaan tilaksi WLAN_CONNECT_SUCCESS
+    WLAN_STATE = WLAN_CONNECT_SUCCESS;
     Serial.println("Wlan yhdistetty onnistuneesti");
+    connectMQTT();//Yhdistäminen onnistui, aloitetaan mqtt
   }
 }
 
 void connectMQTT(){
+  client.setServer(broker,port);
   Serial.println("yhdistetään mqtt...");
+  /* Tarvitsee myös jonkinlaisen tarkistuksen, turha pitää wlania päällä jos mqtt ei yhdistä */
   while(!client.connected()){
-    Serial.print(client.connect("esp32"));
+    Serial.print(client.connect(devname));
     Serial.println(client.state());
-    delay(500);
+    delay(1000);
   }
-   
-   Serial.println("mqtt yhdistetty");
 }
 
-uint16_t readCo2Sensor(){
+void readCo2Sensor(){
   /* Mitataan kaasu */
-  Wire.beginTransmission(i2caddr);
+  Wire.beginTransmission(co2_addr);
   Wire.write(0x36);Wire.write(0x39); //0x3639 measure gas concentration
   Wire.endTransmission();
 
   delay(100); //Tarvitaan viive ennen lukua (saattaa toimia pienemmälläkin)
   
   int i = 0;
-  uint8_t bytes[2]={0,0}; //Voidaan laajentaa jos halutaan lukea myös lämpötila
-  uint16_t result=0;
+  uint8_t bytes[4]={0,0,0,0}; 
+  uint16_t co2_result=0,temp_result=0;
   
   /* Luetaan saadut tiedot */
-  Wire.requestFrom(i2caddr, 2); //luetaan 2 tavua, tiedot kaasusta (ei lämpötilaa)
+  Wire.requestFrom(co2_addr, 4); //luetaan 4 tavua, tiedot ensin kaasusta ja sitten lämpötilasta
   while(Wire.available()){
     bytes[i] = Wire.read(); //Data tulee MSB ensin 
     ++i;
   }
   //liitetään molemmat arvot samaan
-  result = ((uint16_t)bytes[0]<<8) | (uint16_t)bytes[1]; //rullataan ensimmäistä tavua vasemmalle 8 paikkaa ja sitten bittitason OR, jotta saadaan yksi 16 bittinen arvo.
-  Serial.println(result);
+  co2_result = ((uint16_t)bytes[0]<<8) | (uint16_t)bytes[1]; //rullataan ensimmäistä tavua vasemmalle 8 paikkaa ja sitten bittitason OR, jotta saadaan yksi 16 bittinen arvo.
+  temp_result = ((uint16_t)bytes[2] << 8) | (uint16_t)bytes[3]; //sama mutta lämpötilalle
+  
+  //Tarkistusta varten
+  Serial.print("Co2: ");
+  Serial.print(co2_result);
+  Serial.print("\t");
+  Serial.print("Temp: ");
+  Serial.println(temp_result);
 
-  return result;
+  //Lähetetään nyt tiedot vaan suoraan brokerille
+  sendResult(co2_result, co2_topic);
+  sendResult(temp_result, temp_topic);
 }
 
-void loop() {
-  delay(2000);
-
-  /* Mitataan kaasu 
-  Wire.beginTransmission(i2caddr);
-  Wire.write(0x36);Wire.write(0x39); //0x3639 measure gas concentration
-  Wire.endTransmission();
-
-  delay(100); //Tarvitaan viive ennen lukua (saattaa toimia pienemmälläkin)
-  
-  int i = 0;
-  uint8_t bytes[2]={0,0}; //Voidaan laajentaa jos halutaan lukea myös lämpötila
-  uint16_t result=0;
-  
-  /* Luetaan saadut tiedot 
-  Wire.requestFrom(i2caddr, 2); //luetaan 2 tavua, tiedot kaasusta (ei lämpötilaa)
-  while(Wire.available()){
-    bytes[i] = Wire.read(); //Data tulee MSB ensin 
-    ++i;
-  }
-  //liitetään molemmat arvot samaan
-  result = ((uint16_t)bytes[0]<<8) | (uint16_t)bytes[1]; //rullataan ensimmäistä tavua vasemmalle 8 paikkaa ja sitten bittitason OR, jotta saadaan yksi 16 bittinen arvo.
-  Serial.println(result);*/
-
-  uint16_t result = readCo2Sensor();
-
-  int len = snprintf(NULL,0,"%d",result); //Hankitaan mitatun datan pituus
-  char* sResult = (char*)malloc(len*sizeof(char)+1); //Varataan muistia mjonolle jonka pituus on mittaustulos + NULL
-  snprintf(sResult, len+1, "%d", result);//Kirjataan tulos varattuun merkkijonoon
+void sendResult(uint16_t val, char* topic){ 
+  int len = snprintf(NULL,0,"%d",val); //Hankitaan mitatun datan pituus
+  char* sResult = (char*)malloc((len+1)*sizeof(char)); //Varataan muistia mjonolle jonka pituus on mittaustulos + NULL
+  snprintf(sResult, len+1, "%d", val);//Kirjataan tulos varattuun merkkijonoon
  
   client.publish(topic,sResult);//Lähetetään mitattu tieto MQTT brokerille
   
   free(sResult);//Vapautetaan muisti
+}
 
-  
-  //Tarkistusta varten
-  /*Serial.print("\t");
-  Serial.print(bytes[0]);
-  Serial.print(" ");
-  Serial.println(bytes[1]);*/
+void loop() {
+  delay(2000);
+  readCo2Sensor();
 
 }
