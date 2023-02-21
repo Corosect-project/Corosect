233171362d81a161578315a5b874cc831249ac07
Petteri Karjalainen <t0kape02@students.oamk.fi>
2023-02-21 11:34:26 +0200

    Korjattu vikoja koodissa

--- Team_2_ESP32/Arduino_uno_mittauskoodi/Arduino_uno_mittauskoodi.ino
+++ Team_2_ESP32/Arduino_uno_mittauskoodi/Arduino_uno_mittauskoodi.ino
@@ -4,7 +4,7 @@ Program counts arduino uno pinout program current and voltages over resistor.
 */
 
 double voltage = 0.0, current = 0.0, power = 0.0, resistorvalue = 2.2; //Alkuarvo jännitteelle, virta oletus, luodaan resistanssi ja teho. Starting value for voltage, current, resistor and power 
-int voltageport = A0; //Jännitteenlukuportti
+int voltageport = A0; //Jännitteen lukuportti current reading port
 
 void setup() {
   Serial.begin(9600); //Avataan sarjaportti Opening serialport
@@ -14,5 +14,6 @@ void loop() {
   voltage = (analogRead(voltageport) * 5.0) / 1024.0; //U
   current = voltage / resistorvalue; //I=U/R
   power = voltage * current; //P=U*I
-  Serial.println(voltage+" "+current+" "+power); //Jännite, virta ja teho tulostus. Voltage, current and power print.
+  Serial.println(String(voltage) + " " + String(current) + " " + String(power)); //Jännite, virta ja teho tulostus. Voltage, current and power print.
   delay(1000); //Viive delay
+}
