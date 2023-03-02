079be53105a7fa3db8438140cc55c39e0d9a30ca
Jerpism <jere.m@live.fi>
2023-03-02 18:51:18 +0200

    Clean up write and read functions for i2c

--- Team_2_ESP32/no-arduino/main/main.c
+++ Team_2_ESP32/no-arduino/main/main.c
@@ -14,7 +14,7 @@
 #define LED_GPIO 8
 
 /* Set SDA and SCL pins to 6 and 7
- * Default pins incompatible with changing LED color */
+ * Default pins incompatible with internal LED pin */
 #define I2C_MASTER_SDA_IO 6
 #define I2C_MASTER_SCL_IO 7
 
@@ -24,6 +24,8 @@
 /* Address for Click Co2 sensor */
 #define I2C_CO2_ADDR 0x29 
 
+
+static const char* TAG = "espi";
 
 static esp_err_t i2c_master_init(){
     int i2c_master_port = I2C_NUM_0;
@@ -48,135 +50,113 @@ static esp_err_t i2c_master_init(){
 
 }
 
-/* Send command 0x3768 to STC31 sensor
- * disables CRC for data transmissions as it is not required for this project */
-static esp_err_t disable_crc(){
+/* Write 16 bit command to i2c slave */
+static esp_err_t i2c_write(uint8_t addr, uint8_t cmdh, uint8_t cmdl){
     esp_err_t ret;
-    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create(); 
     i2c_master_start(cmd);
-    i2c_master_write_byte(cmd, I2C_CO2_ADDR<<1 | 0x0, 0x1);
-    i2c_master_write_byte(cmd, 0x37, 0x1);
-    i2c_master_write_byte(cmd, 0x68, 0x1);
+    i2c_master_write_byte(cmd, addr<<1 | 0x0, 0x1);
+    i2c_master_write_byte(cmd, cmdh, 0x1);
+    i2c_master_write_byte(cmd, cmdl, 0x1);
+    i2c_master_stop(cmd);
+    ret = i2c_master_cmd_begin(0, cmd, 1000/portTICK_PERIOD_MS);
+//    ESP_LOGI(TAG, "Wrote command, response %s",esp_err_to_name(ret));
+    i2c_cmd_link_delete(cmd);
+
+    return ret;
+}
+
+/* Write 16 bit command to i2c slave with 16 bit argument */
+static esp_err_t i2c_write_with_arg(uint8_t addr, uint8_t cmdh, uint8_t cmdl, uint8_t argh, uint8_t argl){
+    esp_err_t ret;
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create(); 
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, addr<<1 | 0x0, 0x1);
+    i2c_master_write_byte(cmd, cmdh, 0x1);
+    i2c_master_write_byte(cmd, cmdl, 0x1);
+    i2c_master_write_byte(cmd, argh, 0x1);
+    i2c_master_write_byte(cmd, argl, 0x1);
     i2c_master_stop(cmd);
     ret = i2c_master_cmd_begin(0, cmd, 1000/portTICK_PERIOD_MS);
     i2c_cmd_link_delete(cmd);
 
     return ret;
+
 }
 
-/* Send command 0x365B to STC31 sensor
- * runs automatic self-test on sensor and saves result to *data_h and *data_l */
-static esp_err_t self_test(uint8_t *data_h, uint8_t *data_l){
+/* 
+ * Improved i2c write function, work in progress
+ *
+static esp_err_t i2c_write_cmd(uint8_t addr, const uint8_t *data, size_t len){
+    esp_err_t ret;
     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
     i2c_master_start(cmd);
-    i2c_master_write_byte(cmd, I2C_CO2_ADDR<<1 | 0x0, 0x1);
-    i2c_master_write_byte(cmd, 0x36, 0x1); 
-    i2c_master_write_byte(cmd, 0x5B, 0x1);
+    i2c_master_write_byte(cmd, addr << 1 | 0x0, 0x1);
+    i2c_master_write(cmd, data, len, 0x1);
     i2c_master_stop(cmd);
-    esp_err_t ret = i2c_master_cmd_begin(0, cmd, 1000/portTICK_PERIOD_MS);
+    ret = i2c_master_cmd_begin(0, cmd, 1000 / portTICK_PERIOD_MS);
     i2c_cmd_link_delete(cmd);
 
+    return ret;
+}*/
+
+static esp_err_t i2c_read(uint8_t addr, uint8_t *data, size_t len){
+    esp_err_t ret;
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, addr << 1 | 0x1, 0x1);
+    i2c_master_read(cmd, data, len, 0x0);
+    i2c_master_write_byte(cmd, addr<<1 | 0x1, 0x0); /* Seems necessary, otherwise everything past first byte read will be 255 */
+    i2c_master_stop(cmd);
+    ret = i2c_master_cmd_begin(0, cmd, 1000 / portTICK_PERIOD_MS);
+    i2c_cmd_link_delete(cmd);
+
+    return ret;
+
+}
+
+static inline esp_err_t disable_crc(){
+    esp_err_t ret = i2c_write(I2C_CO2_ADDR, 0x37, 0x68);
+    return ret;
+}
+
+
+static inline esp_err_t self_test(uint8_t *data){
+    esp_err_t ret = i2c_write(I2C_CO2_ADDR, 0x36, 0x5B); /* 0x365B run self test */
     if(ret != ESP_OK) return ret;
 
     /* Self-test guaranteed to take <20ms */
     vTaskDelay(20 / portTICK_PERIOD_MS);
-
-    cmd = i2c_cmd_link_create();
-    i2c_master_start(cmd);
-    i2c_master_write_byte(cmd, I2C_CO2_ADDR << 1 | 0x1, 0x1);
-    i2c_master_read_byte(cmd, data_h, 0x0);
-    i2c_master_read_byte(cmd, data_l, 0x1);
-    i2c_master_stop(cmd);
-    ret = i2c_master_cmd_begin(0, cmd, 1000 / portTICK_PERIOD_MS);
-    i2c_cmd_link_delete(cmd);
-
+    ret = i2c_read(I2C_CO2_ADDR, data, 2);
     return ret;
-
 }
 
-/* Send command 0x3615 with arg 0x0001
- * set binary gas to CO2 in air 0 to vol% */
-static esp_err_t set_binary_gas(){
+/* Sets binary gas to co2 in air 0 to 100 vol%*/
+static inline esp_err_t set_binary_gas(){
     esp_err_t ret;
-    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-    i2c_master_start(cmd);
-    i2c_master_write_byte(cmd, I2C_CO2_ADDR<<1 | 0x0, 0x1);
-
-    /* Command */
-    i2c_master_write_byte(cmd, 0x36, 0x1); 
-    i2c_master_write_byte(cmd, 0x15, 0x1); 
-    /* Arg */
-    i2c_master_write_byte(cmd, 0x00, 0x1); 
-    i2c_master_write_byte(cmd, 0x01, 0x1); 
-
-    i2c_master_stop(cmd);
-    ret = i2c_master_cmd_begin(0, cmd, 1000 / portTICK_PERIOD_MS);
-    i2c_cmd_link_delete(cmd);
-
+    ret = i2c_write_with_arg(I2C_CO2_ADDR, 0x36, 0x15, 0x00, 0x01);
     return ret;
 
 }
 
-/* Send command 0x3639 
- * measure gas concentration and read result
- * gas concentration to *co2_data
- * temperature to *temp_data */
-static esp_err_t measure_gas(uint16_t *co2_data, uint16_t *temp_data){
-    uint8_t co2_h,co2_l,temp_h,temp_l;
-
-    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-    i2c_master_start(cmd);
-    i2c_master_write_byte(cmd, I2C_CO2_ADDR<<1 | 0x0, 0x1);
-    i2c_master_write_byte(cmd, 0x36, 0x1); 
-    i2c_master_write_byte(cmd, 0x39, 0x1);
-    i2c_master_stop(cmd);
-    esp_err_t ret = i2c_master_cmd_begin(0, cmd, 1000/portTICK_PERIOD_MS);
-    i2c_cmd_link_delete(cmd);
-
+/* Measure gas and temperature */
+static esp_err_t measure_gas(uint8_t *data){
+    esp_err_t ret = i2c_write(I2C_CO2_ADDR, 0x36 ,0x39);
     if(ret != ESP_OK) return ret;
 
     /* Measurement should take at most 66ms */
     vTaskDelay(70 / portTICK_PERIOD_MS);
-    cmd = i2c_cmd_link_create();
-    i2c_master_start(cmd);
-    i2c_master_write_byte(cmd, I2C_CO2_ADDR<<1 | 0x1, 0x1);
-    i2c_master_read_byte(cmd, &co2_h, 0x0); /* Read high byte for co2 measurement */
-    i2c_master_read_byte(cmd, &co2_l, 0x0); /* Read low byte for co2 measurement */
 
-    i2c_master_read_byte(cmd, &temp_h, 0x0); /* Read high byte for temperature measurement */
-    i2c_master_read_byte(cmd, &temp_l, 0x1); /* Read low byte for temperature measurement */
-    i2c_master_stop(cmd);
-    ret = i2c_master_cmd_begin(0, cmd, 1000 / portTICK_PERIOD_MS);
-    i2c_cmd_link_delete(cmd);
-
-    *co2_data = (uint16_t)co2_h << 8 | (uint16_t)co2_l;
-    *temp_data = (uint16_t)temp_h << 8 | (uint16_t)temp_l;
+    ret = i2c_read(I2C_CO2_ADDR, data, 4);
 
     return ret;
-
-
 }
 
-/*TODO: clean this up, combine sleep and wakeup functions potentially */
 
-/* Send sleep command 0x3677 to sensor
- * otherwise wake device up from sleep by sending addr + write bit */
-static esp_err_t sensor_sleep(){
-    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-    i2c_master_start(cmd);
-    i2c_master_write_byte(cmd, I2C_CO2_ADDR<<1 | 0x0, 0x1);
-        i2c_master_write_byte(cmd, 0x36, 0x1);
-        i2c_master_write_byte(cmd, 0x77, 0x1);
-
-    i2c_master_stop(cmd);
-    esp_err_t ret = i2c_master_cmd_begin(0, cmd, 1000 / portTICK_PERIOD_MS);
-    i2c_cmd_link_delete(cmd);
-
-    /* 12ms wakeup for sensor */
-    vTaskDelay(12 / portTICK_PERIOD_MS);
+/* Send sleep command 0x3677 to sensor*/
+static inline esp_err_t sensor_sleep(){
+    esp_err_t ret = i2c_write(I2C_CO2_ADDR, 0x36, 0x77);
     return ret;
-
-
 }
 
 /* Run wakeup sequence*/
@@ -210,8 +190,8 @@ static void configure_led(){
     gpio_config_t conf ={
         .pin_bit_mask = 0x100, /* Bit mask for GPIO 8 */
         .mode = GPIO_MODE_OUTPUT,
-        .pull_up_en = GPIO_PULLUP_ENABLE,
-        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_ENABLE,
         .intr_type = GPIO_INTR_DISABLE,
     };
 
@@ -221,17 +201,21 @@ static void configure_led(){
 
 
 void initialize_i2c(){
-    uint8_t data_h=1, data_l=1;
+    uint8_t data[2]={0};
 
     int test = i2c_master_init();
     puts("i2c init");
     printf("driver install %s\n",esp_err_to_name(test));
 
-    test = self_test(&data_h, &data_l);
-    printf("selftest %s\n",esp_err_to_name(test));
-    printf("selftest result: %d%d\n",data_h, data_l);
+    test = disable_crc();
+    printf("disable crc %s\n",esp_err_to_name(test));
 
-    sensor_wake();
+    test = set_binary_gas();
+    printf("set binary gas %s\n\n",esp_err_to_name(test));
+
+    test = self_test(data);
+    printf("selftest %s\n",esp_err_to_name(test));
+    printf("selftest result: %d %d\n",data[0], data[1]);
 }
 
 
@@ -241,33 +225,47 @@ void go_to_sleep(int ms){
 
 }
 
-
 void app_main(void){
-    uint16_t co2,temp;
+    uint8_t data[6]={0};
+    int test;
 
     initialize_i2c();
-    configure_led();
+    test = measure_gas(data);
+    printf("Measure: %s\n",esp_err_to_name(test));
+    printf("co2 Data: %d\n",(data[0]<<8|data[1]));
+    printf("temp data: %d\n\n",(data[2]<<8|data[3]));
 
     while(1){
-        sensor_wake();
-        gpio_set_level(LED_GPIO,true);
+        measure_gas(data);
+        printf("co2: %d \t temp: %d\n",(data[0]<<8|data[1]), (data[2]<<8|data[3]));
+//        printf("co2:%d %d %d\t temp: %d %d %d\n",data[0],data[1],data[2],data[3],data[4],data[5]);
 
-        for(int i = 0; i < 10; i++){
-            measure_gas(&co2,&temp);
-            printf("%hu \t %hu\n",co2,temp);
-            vTaskDelay(500 / portTICK_PERIOD_MS);
-        }
-
-        vTaskDelay(500 / portTICK_PERIOD_MS);
-
-        puts("Going to sleep");
-        gpio_set_level(LED_GPIO,false);
-        sensor_sleep();
-        go_to_sleep(5000);
-
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
 
     }
+
+    //    configure_led();
+
+    /*    while(1){
+          sensor_wake();
+          gpio_set_level(LED_GPIO,true);
+
+          for(int i = 0; i < 10; i++){
+          measure_gas(&co2,&temp);
+          printf("%hu \t %hu\n",co2,temp);
+          vTaskDelay(500 / portTICK_PERIOD_MS);
+          }
+
+          vTaskDelay(500 / portTICK_PERIOD_MS);
+
+          puts("Going to sleep");
+          gpio_set_level(LED_GPIO,false);
+          sensor_sleep();
+          go_to_sleep(5000);
+
+          vTaskDelay(5000 / portTICK_PERIOD_MS);
+
+          }*/
 
 }
 
