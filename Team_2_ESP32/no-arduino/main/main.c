9c2f0279ac954ce0d45621a0cf42cb9e6a2fae25
Petteri Karjalainen <t0kape02@students.oamk.fi>
2023-03-16 12:22:52 +0200

    Päivitetty uudet koodit

--- Team_2_ESP32/no-arduino/main/main.c
+++ Team_2_ESP32/no-arduino/main/main.c
@@ -22,17 +22,30 @@
 #define I2C_MASTER_SCL_IO 7
 
  RTC maximum SCL frequency is 1MHz 
-#define I2C_MASTER_FREQ_HZ 1000000
+#define I2C_MASTER_FREQ_HZ 1000000*/
 
- Address for Click Co2 sensor */
+/* Address for Click Co2 sensor */
 #define I2C_CO2_ADDR CONFIG_I2C_CO2_ADDR
+
+/* Number of measurements every cycle */
+#define SAMPLES CONFIG_SAMPLES
+
+/* Time between each measurement (ms). Should be >= 1000 */
+#define MEASURE_INTERVAL CONFIG_MEASURE_INTERVAL
+
+/* Sleep time between succesful measurements (ms) */
+#define SLEEP_MS CONFIG_SLEEP_MS
+
+/* Sleep time in case of a failure (ms) */
+#define ERR_SLEEP_MS CONFIG_ERR_SLEEP_MS
+
 
 
 static const char* TAG = "espi";
 
 
-/* MQTT client used to send messages */
-esp_mqtt_client_handle_t mqtt_client;
+/* MQTT client used to send messages  
+esp_mqtt_client_handle_t mqtt_client;*/
 
 static inline esp_err_t disable_crc(){
     esp_err_t ret = i2c_write(I2C_CO2_ADDR, 0x37, 0x68);
@@ -77,9 +90,17 @@ static inline esp_err_t sensor_sleep(){
     return ret;
 }
 
-static void configure_led(){
+static inline esp_err_t sensor_wake(){
+    esp_err_t ret = i2c_wakeup_sensor(I2C_CO2_ADDR);
+    /* Sensor needs time to wake up, 50ms seems reliable */
+    vTaskDelay(50 / portTICK_PERIOD_MS); 
+    return ret;
+
+}
+
+/*static void configure_led(){
     gpio_config_t conf ={
-        .pin_bit_mask = 0x100, /* Bit mask for GPIO 8 */
+        .pin_bit_mask = 0x100,  Bit mask for GPIO 8 
         .mode = GPIO_MODE_OUTPUT,
         .pull_up_en = GPIO_PULLUP_DISABLE,
         .pull_down_en = GPIO_PULLDOWN_ENABLE,
@@ -88,14 +109,17 @@ static void configure_led(){
 
     gpio_config(&conf);
 
-}
+}*/
 
 void initialize_i2c(){
-    uint8_t data[2]={0};
+    uint8_t data[2]={-1};
 
     int test = i2c_master_init();
     puts("i2c init");
     printf("driver install %s\n",esp_err_to_name(test));
+
+    test = sensor_wake();
+    printf("sensor wake %s\n",esp_err_to_name(test));
 
     test = disable_crc();
     printf("disable crc %s\n",esp_err_to_name(test));
@@ -104,25 +128,17 @@ void initialize_i2c(){
     printf("set binary gas %s\n\n",esp_err_to_name(test));
 
     test = self_test(data);
-    printf("selftest %s\n",esp_err_to_name(test));
+    if((data[0] << 8 | data[1]) != 0){ ESP_LOGE(TAG,"Sensor selftest failure");};
+    printf("selftest sent %s\n",esp_err_to_name(test));
     printf("selftest result: %d %d\n",data[0], data[1]);
 }
 
 
 void go_to_sleep(int ms){
+    sensor_sleep();
     esp_sleep_enable_timer_wakeup(ms*1000);
     esp_deep_sleep_start();
-
 }
-
-/*void sendResult(uint16_t val, char *topic){
-    TODO: Replace this with a static buffer at some point
-    int len = snprintf(NULL,0,"%d",val); 
-    char* sResult = (char*)malloc((len+1)*sizeof(char));
-    snprintf(sResult, len+1, "%d", val);
-
-    esp_mqtt_client_publish(mqtt_client, topic, sResult, len, 0, 0);
-}*/
 
 void app_main(void){
     uint8_t data[4]={0};
@@ -130,25 +146,39 @@ void app_main(void){
     uint16_t temp_result;
     int test;
 
+    nvs_flash_init();
+    test = wifi_init_sta();
+    /* wifi_init_sta() will return 0 on success
+     * otherwise connecting failed */
+    if(test != 0){
+        ESP_LOGI(TAG,"Wifi connection failed, sleeping");
+        go_to_sleep(ERR_SLEEP_MS);
+    }
+
+    ESP_LOGI(TAG,"Got wifi connection, starting MQTT");
+    mqtt_app_start();
     initialize_i2c();
     test = measure_gas(data);
     printf("Measure: %s\n",esp_err_to_name(test));
     printf("co2 Data: %d\n",(data[0]<<8|data[1]));
     printf("temp data: %d\n\n",(data[2]<<8|data[3]));
-    nvs_flash_init();
-    wifi_init_sta();
-    mqtt_app_start();
+
 
     while(1){
-        measure_gas(data);
-        co2_result = data[0] << 8 | data[1];
-        temp_result = data[2] << 8 | data[3];
-        mqtt_send_result(co2_result, "co2");
-        mqtt_send_result(temp_result, "temp");
-        printf("co2: %d \t temp: %d\n",(data[0]<<8|data[1]), (data[2]<<8|data[3]));
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        for(int i = 0; i < SAMPLES; i++){
+            measure_gas(data);
+            co2_result = data[0] << 8 | data[1];
+            temp_result = data[2] << 8 | data[3];
+            mqtt_send_result(co2_result, "co2");
+            mqtt_send_result(temp_result, "temp");
+            printf("co2: %d \t temp: %d\n",(data[0]<<8|data[1]), (data[2]<<8|data[3]));
+            /* Measure should only be called once every second */
+            vTaskDelay(MEASURE_INTERVAL / portTICK_PERIOD_MS);
+        }
+
+        ESP_LOGI(TAG,"Measured, going to sleep");
+        go_to_sleep(SLEEP_MS);
+
 
     }
-
 }
-
