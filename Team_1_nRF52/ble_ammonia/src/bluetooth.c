bbffad3b49be8d84c40972e3a60f5b1276b22c22
Astru43 <45196326+Astru43@users.noreply.github.com>
2023-04-14 07:05:44 +0300

    Add BLE GATT service base

--- Team_1_nRF52/ble_ammonia/src/bluetooth.c
+++ Team_1_nRF52/ble_ammonia/src/bluetooth.c
@@ -2,6 +2,7 @@
 
 #include <zephyr/bluetooth/bluetooth.h>
 #include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/gatt.h>
 #include <zephyr/bluetooth/uuid.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
@@ -9,9 +10,24 @@ LOG_MODULE_DECLARE(app, LOG_LEVEL_DBG);
 
 #define ERROR(err) (err < 0)
 
+ssize_t read_test(struct bt_conn *conn,
+                  const struct bt_gatt_attr *attr,
+                  void *buf, uint16_t len,
+                  uint16_t offset) {
+  void *value = attr->user_data;
+  // LOG_DBG("%s", value);
+  return bt_gatt_attr_read(conn, attr, buf, len, offset, value, strlen(value));
+}
+
+// static const struct bt_gatt_service service =
+static const struct bt_uuid_16 fixed_str_16_uuid = BT_UUID_INIT_16(0x2af5);
+BT_GATT_SERVICE_DEFINE(
+    IDK,
+    BT_GATT_PRIMARY_SERVICE(BT_UUID_ESS),
+    BT_GATT_CHARACTERISTIC(&fixed_str_16_uuid.uuid, BT_GATT_CHRC_READ, BT_GATT_PERM_READ, read_test, NULL, "Hello, world   "), );
 static const struct bt_data ad[] = {
     BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
-    BT_DATA_BYTES(BT_DATA_UUID16_ALL, BT_UUID_16_ENCODE(0xefaa)),
+    BT_DATA_BYTES(BT_DATA_UUID16_ALL, BT_UUID_16_ENCODE(BT_UUID_ESS_VAL)),
 };
 
 void bt_ready();
@@ -23,12 +39,18 @@ BT_CONN_CB_DEFINE(con_calbacks) = {
     .disconnected = bt_disconnected};
 
 int start_bt() {
+  struct bt_gatt_attr *attr = bt_gatt_find_by_uuid(NULL, 0, (struct bt_uuid *)&fixed_str_16_uuid);
+  if (attr) {
+    LOG_DBG("Attr found, value %s", (char *)attr->user_data);
+  }
+
   int err = bt_enable(NULL);
   if (ERROR(err)) {
     LOG_ERR("Error enabling BT %d", errno);
     return err;
   } else
     bt_ready();
+  return 0;
 }
 
 void stop_bt() {
